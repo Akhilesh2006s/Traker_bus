1182e48fe42257f46de6c3d271c1214b6a6499d9
sravanidasari4488 <154404196+sravanidasari4488@users.noreply.github.com>
2025-03-29 20:54:56 +0530

    Update Login.tsx

--- app/Login.tsx
+++ app/Login.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useState} from "react";
 import { View, Text, StyleSheet, TouchableOpacity, Image } from "react-native";
 import { useOAuth, useAuth } from "@clerk/clerk-expo"; // Import useAuth
 import { useRouter } from "expo-router";
@@ -8,13 +8,17 @@ const Login = () => {
   const { signOut } = useAuth(); // Get signOut from Clerk
   const { startOAuthFlow } = useOAuth({ strategy: "oauth_google" });
 
+  const [errorMessage, setErrorMessage] = useState("");
+
   const handleLogin = async () => {
     try {
       await signOut(); // Ensure any existing session is logged out before login
+      setErrorMessage("");
+
       const { createdSessionId, signIn } = await startOAuthFlow();
 
-      if (createdSessionId) {
-        router.replace("/NextPage2"); // Redirect to the home page
+      if (createdSessionId ) {
+        router.replace("/Homepage2"); // Redirect to the home page
       } else {
         console.error("OAuth flow failed", signIn);
       }
@@ -25,6 +29,8 @@ const Login = () => {
 
   return (
     <View style={styles.container}>
+
+      
       <Image source={require("@/assets/images/Vitapbuilding.jpeg")} style={styles.backgroundImage} />
       <View style={styles.overlay}>
         <Image source={require("@/assets/images/vit-ap.png")} style={styles.logo} />
